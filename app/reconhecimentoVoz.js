b3475dda20c492651dbd241c12a1a1d44aedfa46
Vagnes-silva <100873980+Vagnes-silva@users.noreply.github.com>
2025-09-23 14:11:09 -0300

    Add files via upload

--- app/reconhecimentoVoz.js
+++ app/reconhecimentoVoz.js
@@ -1,4 +1,6 @@
-const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const elementoChute = document.getElementById('chute')
+
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
 recognition.lang = 'pt-Br'
@@ -6,6 +8,17 @@ recognition.start()
 
 recognition.addEventListener('result', onSpeak)
 
-function onSpeak(e){
-    console.log(e)
+function onSpeak(e) {
+    chute = e.results[0][0].transcript
+    exibeChuteNaTela(chute)
+    verificaSeOChutePossuiUmValorValido(chute)
 }
+
+function exibeChuteNaTela(chute) {
+    elementoChute.innerHTML = `
+        <div>Você disse</div>
+        <span class="box">${chute}</span>
+     `
+}
+
+recognition.addEventListener('end', () => recognition.start())
